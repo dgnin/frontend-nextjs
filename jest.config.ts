7784eb7c70b47b123f6268ffe37c8205734f252b
dgnin <davidgomeznin@protonmail.com>
2024-11-26 12:15:14 +0100

    Adds paths to configuration, after deciding more or less how we're gonna structure the code (possible changes in the future)

--- jest.config.ts
+++ jest.config.ts
@@ -8,6 +8,13 @@ const createJestConfig = nextJest({
 const config: Config = {
   testEnvironment: 'jsdom',
   testRegex: '/src/.*\\.test\\.(ts|tsx)$',
+  moduleNameMapper: {
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+    '^@icons/(.*)$': '<rootDir>/src/assets/icons/$1',
+    '^@styles/(.*)$': '<rootDir>/src/assets/styles/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@modules/(.*)$': '<rootDir>/src/modules/$1',
+  },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
 
